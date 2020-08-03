b2a2e3a963fbf13d23b8b2351023534c60f1613c
Kim jeong tae <tae101010@naver.com>
2020-08-03 13:31:33 +0900

    단어장 추가 대화상자

--- app/src/main/java/kr/ac/kpu/mywordbook/myWordBookFragment.kt
+++ app/src/main/java/kr/ac/kpu/mywordbook/myWordBookFragment.kt
@@ -1,9 +1,11 @@
 package kr.ac.kpu.mywordbook
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragmywordbook.*
@@ -13,8 +15,26 @@ class myWordBookFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragmywordbook, container,false)
 
-
-
+        setHasOptionsMenu(true)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu,inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu,inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.newmemo -> {
+                val dlgView = layoutInflater.inflate(R.layout.wordbookname, null)
+                val btn_picture : TextView = dlgView.findViewById(R.id.tv_name)
+                val btn_text : EditText = dlgView.findViewById(R.id.ed_name)
+                val dlgBuilder = AlertDialog.Builder(activity)
+                dlgBuilder.setView(dlgView)
+                dlgBuilder.show()
+            }
+        }
+        return false
     }
 }
