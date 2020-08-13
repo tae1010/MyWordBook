ae90b5c103d50c41eb7129cec01f0c7b2f30f08f
Kim jeong tae <tae101010@naver.com>
2020-08-13 17:05:00 +0900

    전체적으로 마무리 수정 + 앱 아이콘/앱 이름 제작

---
+++ app/src/main/java/kr/ac/kpu/mywordbook/QuizActivity.kt
@@ -0,0 +1,103 @@
+package kr.ac.kpu.mywordbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_quiz.*
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+
+        val database = Firebase.database
+
+        var wList=arrayListOf<ListWord>()
+        var krList = arrayListOf<String>()    //모든 뜻을 저장
+        var answerList = arrayListOf<String>()    //버튼에 들어갈 4개의 뜻
+
+        val title = intent.getStringExtra("title")
+        val date = intent.getStringExtra("date")
+        val email = intent.getStringExtra("email")
+        var count: Int = 0  //문제 갯수
+        val myRef1 = database.getReference("users/$email/$date/$title")
+        myRef1.addListenerForSingleValueEvent(object : ValueEventListener {
+            fun setTest(count:Int, list:ArrayList<String>){
+
+
+            }
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                var answer: String? =null
+                for (snapshot in p0.children) {
+                    wList.add(
+                        ListWord(
+                            "${snapshot.key.toString()}",
+                            "${snapshot.value.toString()}"
+                        )
+                    )
+                }
+                for (i in 0 until wList.size) {
+                    krList.add(wList[i].krWord.toString())
+                }// 영단어중 뜻만 모여있는 arrayList
+                wList.shuffle()
+                krList.shuffle()
+                answerList.add(wList[count].krWord.toString())
+                answerList.add(krList[0])
+                answerList.add(krList[1])
+                answerList.add(krList[2])
+                answerList.shuffle()
+
+                textWord.text=wList[count].egWord.toString()
+                answer=wList[count].krWord.toString()
+                button1.text=answerList[0]
+                button2.text=answerList[1]
+                button3.text=answerList[2]
+                button4.text=answerList[3]
+
+                button1.setOnClickListener {
+                    if(answer==button1.text){
+                        textWord.text="정답"
+                        answerList.clear()
+                    }
+                }
+                button2.setOnClickListener {
+                    if(answer==button2.text){
+                        textWord.text="정답"
+                        answerList.clear()
+                    }
+                }
+                button3.setOnClickListener {
+                    if(answer==button3.text){
+                        textWord.text="정답"
+                        answerList.clear()
+                    }
+                }
+                button4.setOnClickListener {
+                    if(answer==button4.text){
+                        textWord.text="정답"
+                        answerList.clear()
+                    }
+                }
+                btnNext.setOnClickListener {
+                    val intent = intent
+                    finish()
+                    startActivity(intent)
+                }
+
+                wList.clear()
+            }//ondataChange 끝
+        })
+
+
+
+
+    }  //onCreate method
+
+}
--- app/src/main/java/kr/ac/kpu/mywordbook/QuizFragment.kt
+++ app/src/main/java/kr/ac/kpu/mywordbook/QuizFragment.kt
@@ -1,23 +1,93 @@
 package kr.ac.kpu.mywordbook
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragquiz.*
 import kr.ac.kpu.mywordbook.R
 
 class QuizFragment : Fragment() {
 
+    val database= Firebase.database
+    lateinit var listview : ListView
+    lateinit var adapter: WordBookAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragquiz, container,false)
 
 
+        setHasOptionsMenu(true)
 
+
+        listview = view!!.findViewById(R.id.lv_wordbook) as ListView
+
+
+        registerForContextMenu(listview)
+
+
+        //adapter.notifyDataSetChanged() //삭제하거나 추가할때 리스트뷰 갱신
+
+        //Toast.makeText(activity,"$email",Toast.LENGTH_SHORT).show()
+        val email = activity!!.intent.getStringExtra("email")
+        val myRef1 = database.getReference("users/$email")
+
+        val intent2 = Intent(activity, SharedFragment::class.java)
+        intent2.putExtra("email2",email)
+        //Toast.makeText(activity, "$email", Toast.LENGTH_SHORT).show()
+
+        myRef1.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+
+                for (snapshot in p0.children) {
+                    val myRef2 = database.getReference("users/$email/${snapshot.key}")
+                    myRef2.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onCancelled(p0: DatabaseError) {
+                        }
+
+                        override fun onDataChange(p0: DataSnapshot) {
+
+                            adapter = WordBookAdapter()
+                            listview.adapter = adapter
+
+                            for (snapshot in p0.children) {
+                                wbList.add(ListWordBook("${snapshot.key.toString()}", "${myRef2.key.toString()}"))
+                                //Toast.makeText(activity,"${snapshot.key.toString()}",Toast.LENGTH_SHORT).show()
+                            }
+
+                            for (i in 0 until wbList.size) {
+                                adapter.addItem("${wbList[i].title}", "${wbList[i].date}")
+                                //Log.d(ContentValues.TAG, "###############################")
+                            }
+                        }
+                    })
+                }
+            }
+        })
+        listview.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val intent = Intent(activity, QuizActivity::class.java)
+                intent.putExtra("email",email)
+                intent.putExtra("title", wbList[position].title)
+                intent.putExtra("date", wbList[position].date)
+                startActivity(intent)
+            }
+        wbList.clear()
         return view
     }
 }
